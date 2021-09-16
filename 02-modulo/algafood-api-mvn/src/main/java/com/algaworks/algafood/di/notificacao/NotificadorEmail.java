39e3ca222cb7016f9d84fe9cfbd9b736b2cb20eb
Rafael Barros <rafaelbarros.df@gmail.com>
2021-09-15 21:15:55 -0300

    :construction: Definindo beans @Component

--- 02-modulo/algafood-api-mvn/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ 02-modulo/algafood-api-mvn/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NotificadorEmail implements Notificador {
 
     @Override
--- 02-modulo/algafood-api-mvn/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ 02-modulo/algafood-api-mvn/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -2,8 +2,9 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
     private Notificador notificador;
